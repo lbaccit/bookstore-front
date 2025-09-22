94d43e76434f35285470a991afe5ebdea545339a
Luciana Bacci <l.bacci@uniandes.edu.co>
2025-09-21 22:53:48 -0500

    cambios pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-12">
-      <h1 className="text-4xl font-bold mb-12">Bienvenidos al Bookstore</h1>
+      <h1 className="text-4xl font-bold mb-12">Bienvenidos al Bookstore!</h1>
       <div className="p-4 border rounded-lg m-4 text-center">
         <p className="text-xl mb-2"> En esta página podrás gestionar la información de los autores del Bookstore mediante	un CRUD	
 (Crear,	Leer,	Actualizar	y	Eliminar)</p>
